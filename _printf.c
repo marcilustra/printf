b9bbbb1b9b1670a39f6a9ab347e810c4fb965753
marcilustra <theknifebox69@gmail.com>
2023-01-25 11:53:27 +0300

    _printf

---
+++ _printf.c
@@ -0,0 +1,49 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+  * _printf - print string as per specified format (see format_selector).
+  * @format: format (i, c, s)
+  * Return: output size(Succ)
+  */
+
+int _printf(const char *format, ...)
+{
+	int _charout = 0;
+	unsigned int i = 0;
+	va_list ap;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[i])
+	{
+		while (format[i] != '%' && format[i])
+		{
+			_putchar(format[i]);
+			_charout++;
+			i++;
+		}
+		if (format[i] == '\0')
+			return (_charout);
+		f = format_select(&format[i + 1]);
+		if (f != NULL)
+		{
+			_charout += f(ap);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		_charout++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(ap);
+	return (_charout);
+}
---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - print a character (Standard Output)
+ * @c: Char to print
+ * Return: 1 (Succ
+ * -1 (no Succ)
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
