bc504868c433f72318febbb950d50153b1025687
marcilustra <theknifebox69@gmail.com>
2023-01-26 15:51:38 +0300

    Undo Redo

--- main.h
+++ main.h
@@ -9,6 +9,8 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list c);
 int print_string(va_list s);
+int print_decimal(va_list d);
+int print_int(va_list i)
 
 /**
  * struct string_format - Structure
